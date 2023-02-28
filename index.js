56d22fb0b661fd106fd78acd42c672c3390c0a34
LeAndre Jones <jonsey@leandres-air.home>
2023-02-28 13:25:12 -0600

    Second commit worked on styling web page

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-let reactButton = document.getElementById('react');
-let hiddenReact = document.querySelector('.react');
+// let headName = document.getElementById('heading-name').textContent;
 
-let textInfo = document.querySelector('.infoText');
-reactButton.addEventListener('click', function(){
-    textInfo.textContent = 'This is some info about react.';
-    textInfo.classList.remove('d-none');
-    hiddenReact.classList.remove('d-none');
-});
-
-// function scrollFunction(){
-//     document.querySelector('nav').classList.add('bg-black');
+// function loop(passed){
+//     passed.addEventListener('mouseover',()=>{
+//         console.log('hoverd')
+//     })
+// }
+// for(let i=0; i<headName.length; i++){
+//     // console.log(headName[i]);
+//     // headName[i].addEventListener('click', ()=>{
+//     //     // headName[i].style.color = 'red'
+//     //     console.log(hover)
+//     // })
+//     let item = headName[i];
+//     loop(item)
 // }
 
-// const checkpoint = 300;
-// window.addEventListener("scroll", () => {
-//   const currentScroll = window.pageYOffset;
-//   if (currentScroll <= checkpoint) {
-//     opacity = 0 + currentScroll / checkpoint;
-//   } else {
-//     opacity = 1;
-//   }
-//   document.querySelector("nav").style.opacity = opacity;
-// });
-//this works nice all I have to do tomorrow or later today is make sure the image displays also when the button is clicked
-// I also want to start stylizing more of the site to make it look better because the temporary styles or disgusting
-// good work though me keep it up and we can have this site completed before this weekend!!!
+// // console.log(headName.length)
+
+$('#heading-name').hover(
+  function () {
+    $(this).css('background-color', 'yellow');
+  },
+  function () {
+    $(this).css('background-color', 'pink');
+  }
+);
