030c31b04d6be1016fb9c319713c311a86cd5e77
LeAndre Jones <jonsey@LeAndres-MacBook-Air.local>
2023-03-01 14:27:57 -0600

    added new changes

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-// let headName = document.getElementById('heading-name').textContent;
+let iconInfo = document.getElementById('react');
+let boxDisplay = document.getElementById('box')
 
-// function loop(passed){
-//     passed.addEventListener('mouseover',()=>{
-//         console.log('hoverd')
-//     })
-// }
-// for(let i=0; i<headName.length; i++){
-//     // console.log(headName[i]);
-//     // headName[i].addEventListener('click', ()=>{
-//     //     // headName[i].style.color = 'red'
-//     //     console.log(hover)
-//     // })
-//     let item = headName[i];
-//     loop(item)
-// }
-
-// // console.log(headName.length)
-
-$('#heading-name').hover(
-  function () {
-    $(this).css('background-color', 'yellow');
-  },
-  function () {
-    $(this).css('background-color', 'pink');
-  }
-);
+iconInfo.addEventListener('click',()=>{
+  boxDisplay.classList.remove('d-none')
+})
